86921c2f3fabd3f09bfc87b439d38fa34acdb39b
DESKTOP-D515R4N\Luka <lukalijevic@gmail.com>
2018-05-25 23:19:07 +0200

    no message

---
+++ Hood/Assets/Scripts/NPCAttack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAttack : MonoBehaviour {
+
+    public float WaitBeweenAttacks = 2.0f;
+
+    public bool ShouldFollow = false;
+
+    private float _stopwatch = 0.0f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            ShouldFollow = true;
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            _stopwatch += Time.deltaTime;
+            if (_stopwatch >= WaitBeweenAttacks)
+            {
+                //TODO: hpmanager i animator
+              /*  anim.SetFloat("MoveX", Mathf.Sign(myRigidbody.velocity.x));
+                if (myRigidbody.velocity.x == 0)
+                    anim.SetFloat("MoveX", 0.0f);
+                anim.SetFloat("MoveY", Mathf.Sign(myRigidbody.velocity.y));
+                if (myRigidbody.velocity.y == 0)
+                    _stopwatch = 0.0f;*/
+            }
+        }
+    }
+
+    //TODO: vidjet dal želiš
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            _stopwatch = 0.0f;
+        }
+    }
+}
---
+++ Hood/Assets/Scripts/NPCFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCFollow : MonoBehaviour {
+
+    private GameObject _player;
+    public float Speed = 3.0f;
+    private NPCAttack _npcAttack;
+
+    private void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _npcAttack = transform.GetComponentInParent<NPCAttack>();
+    }
+
+    private void Update()
+    {
+        if (_npcAttack.ShouldFollow)
+        {
+            transform.LookAt(_player.transform);
+            transform.position += transform.forward * Speed * Time.deltaTime;
+        }
+    }
+}
